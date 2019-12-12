f65d5f86abe3a73fc3f6c7712d980fe68b10eb03
Alexander Daza <dev.alexander.daza@gmail.com>
2019-12-12 11:27:56 -0500

    New fetures
    * Send email with Gmail or SMTP with nodemailer
    * Add sendApiExceptionResponse method for API
    * Add JSONResponseValidateParameter method for validate parameters in API

---
+++ src/controllers/developer/indexController.ts
@@ -0,0 +1,13 @@
+/**
+ * ==================================================================
+ * Copyright © Dinámica Digital S.A.S.
+ * Author: Neyib Alexander Daza Guerrero
+ * Email: dev.alexander.daza@gmail.com
+ * ==================================================================
+ */
+
+class IndexController {
+
+}
+
+export const indexController = new IndexController();
